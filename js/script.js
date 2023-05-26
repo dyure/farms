36f3e7f18dc4fd4d10de1180cfcde73b354a5c9d
Dmitry Yurkovsky <sobranieinfo@yandex.ru>
2023-05-27 00:10:00 +0700

    fourteenth day forms and bugs

--- js/script.js
+++ js/script.js
@@ -1,113 +1,78 @@
 "use strict";
 
-//после полной загрузки документа
-document.addEventListener('DOMContentLoaded', function(){
-    document.addEventListener('click', e => {
-        if(e.target.closest('input') != null){
-            $('.input_phone').inputmask({
-                "mask" : "+7 (999) 999-99-99"
-            });
-            //id input по которому кликнули
-            const clicked = e.target.closest('input');
-            const inputID = $(clicked).attr('id');
-            //alert(clicked);
-            //id сообщения об ошибке
-            const errorID = '#error_input' + inputID;
-            //id кнопки
-            const buttonID = '#button_phone' + inputID;
-            //id формы
-            const formID = 'form' + inputID;
-            //создаем реакцию на ввод каждого символа
-            const inputPhone = document.getElementById(inputID);
-            //функция подсчета количества введенных символов
-            inputPhone.oninput = function() {
-                if ((inputPhone.value.replace(/[\D]+/g, '').length) == 11) {
-                    $(buttonID).addClass('active');
-                    $(errorID).removeClass('active');
-                } else {
-                    $(buttonID).removeClass('active');
-                    $(errorID).addClass('active');
-                }
-            }
-        
-            const form = document.getElementById(formID);
-            //перехватываем нажатие кнопки
-            form.addEventListener('submit', formSend);
-            //отправка формы если нет ошибок и количество символов 11
-            async function formSend(e){
-                e.preventDefault();
-                let error = formValidate(form);
-                let formData = new FormData(form);
-        
-                if (error === 0 && $(buttonID).hasClass('active')) {
-                    form.classList.add('_sending');
-                    const response = await fetch('sendmail.php', {
-                        method: 'POST',
-                        body: formData
-                    });
-                    
-                    if (response.ok){
-                        //let result = await response.json();
-                        // alert(response.ok);
-                        //form.reset();
-                        form.classList.remove('_sending');
-                        //$(buttonID).removeClass('active');
-                        $('.form_phone').addClass('unactive');
-                        $('.success').addClass('active');
-                        $('.form_title').html('Ваша заявка успешно<br>отправлена');
-                    } else {
-                        //alert ('Ошибка!');
-                        form.classList.remove('_sending');
-                        $(buttonID).removeClass('active');
-                    }
-                } else {
-                    // alert('Заполните поле правильно!');
-                    $(errorID).addClass('active');
-                }
-            }
-            function formValidate(form) {
-                let error = 0;
-                //поместим в переменную formRequired все обязательные поля (с классом _required)
-                let formRequired = form.querySelectorAll('._required');
-                //цикл по обязательным полям
-                for (let index = 0; index < formRequired.length; index++){
-                    const input = formRequired[index];
-                    formRemoveError(input);
-                    if (input.classList.contains('input_phone')) {
-                        if (phoneTest(input)){
-                            //formAddError(input);
-                            error++;
-                        }
-                    } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
-                        formAddError(input);
-                        error++;
-                    } else {
-                        if (input.value === '') {
-                            //formAddError(input);
-                            error++;
-                        }
-                    }
-                }
-                return error;
-            }
-    
-            //добавим класс _error не правильно заполненному полю
-            function formAddError(input){
-                // input.parentElement.classList.add('_error');
-                // input.classList.add('_error');
-            }
-        
-            //уберем класс _error из правильно заполненного поля
-            function formRemoveError(input){
-                // input.parentElement.classList.remove('_error');
-                // input.classList.remove('_error');
-                $(errorID).removeClass('active');
-            }
-        
-            //проверка телефона
-            function phoneTest(input) {
-                return !/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(input.value);
+document.addEventListener('DOMContentLoaded', () => {
+    $('.input_phone').inputmask({
+        "mask" : "+7 (999) 999-99-99"
+    });
+
+    let forms = document.getElementsByTagName('form');
+    for (let i = 0; i < forms.length; i++) {
+        const buttonID = '#button_phone' + forms[i][0].id;
+        const inputPhone = document.getElementById(forms[i][0].id);
+        inputPhone.oninput = function() {
+            if ((inputPhone.value.replace(/[\D]+/g, '').length) == 11) {
+                $(buttonID).addClass('active');
+            } else {
+                $(buttonID).removeClass('active');
             }
         }
-    });
+    }
+
+    const form1 = document.getElementById('form1');
+    form1.addEventListener('submit', formSend);
+    const form2 = document.getElementById('form2');
+    form2.addEventListener('submit', formSend);
+    const form3 = document.getElementById('form3');
+    form3.addEventListener('submit', formSend);
+    const form4 = document.getElementById('form4');
+    form4.addEventListener('submit', formSend);
+    
+    async function formSend(e){
+        e.preventDefault();
+        let formData;
+        const ID = e.srcElement[0].id;
+        switch(ID){
+            case '1':
+                formData = new FormData(form1);
+            break;
+            case '2':
+                formData = new FormData(form2);
+            break;
+            case '3':
+                formData = new FormData(form3);
+            break;
+            case '4':
+                formData = new FormData(form4);
+            break;
+        }
+        
+        if ($('#button_phone' + ID).hasClass('active')) {
+            $('#form' + ID).addClass('_sending');
+            const response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            
+            if (response.ok){
+                $('#form' + ID).removeClass('_sending');
+                if (ID == 1 || ID == 4) {
+                    $('#success1').addClass('active');
+                    $('#success4').addClass('active');
+                    $('.form_phone').addClass('unactive');
+                    $('#form_title1').html('Ваша заявка успешно<br>отправлена');
+                    $('#form_title4').html('Ваша заявка успешно<br>отправлена');
+                } else {
+                    $('#success' + ID).addClass('active');
+                    $('#form2').addClass('unactive');
+                    $('#form3').addClass('unactive');
+                    $('#form_title' + ID).html('Ваша заявка успешно<br>отправлена');
+                }
+            } else {
+                $('#form' + ID).removeClass('_sending');
+                $('#button_phone' + ID).removeClass('active');
+            }
+        } else {
+            $('#error_input' + ID).addClass('active');
+        }
+    }
 });
